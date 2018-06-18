4cabc0b312bed7986c0340ef3412e9ed52ccb5ab
amir.k <amir.k@taboola.com>
2018-06-18 23:36:22 +0300

    preprocessing

--- helper.py
+++ helper.py
@@ -1,15 +1,46 @@
-import pandas as pd
 import re
+import string
+
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+def remove_non_letters(data):
+    regex = re.compile('[^a-zA-Z ]')
+    data['subject'] = [regex.sub(' ', x) for x in data['subject']]
+    data['content'] = [regex.sub(' ', x) for x in data['content']]
+
+def remove_stopwords(data):
+    stopwords_set = set(stopwords.words('english'))
+    data['subject'] = [[y if y not in stopwords_set else '' for y in x] for x in data['subject']]
+    data['subject'] = [filter(None, x) for x in data['subject']]
+    data['content'] = [[y if y not in stopwords_set else '' for y in x] for x in data['content']]
+    data['content'] = [filter(None, x) for x in data['content']]
+
+def stemming(data):
+    ps = PorterStemmer()
+    data['subject'] = [[str(ps.stem(y)) for y in x] for x in data['subject']]
+    data['content'] = [[str(ps.stem(y)) for y in x] for x in data['content']]
+
+def remove_punctuation(data):
+    data['subject'] = [x.translate(None, string.punctuation) for x in data['subject']]
+    data['content'] = [x.translate(None, string.punctuation) for x in data['content']]
+
+def tokenize(data):
+    data['subject'] = [word_tokenize(x) for x in data['subject']]
+    data['content'] = [word_tokenize(x) for x in data['content']]
+
+def lowercase(data):
+    data['subject'] = [x.lower() for x in data['subject']]
+    data['content'] = [x.lower() for x in data['content']]
 
 def get_train_test(train_input, test_input):
-    with open(train_input) as f:
-        lines = f.readlines()
-    lines = [x.strip() for x in lines] 
-
     subject_regex = '\<subject\>(.+)\<\/subject\>'
     content_regex = '\<content\>(.+)\<\/content\>'
     maincat_regex = '\<maincat\>(.+)\<\/maincat\>'
-
+    with open(train_input) as f:
+        lines = f.readlines()
+    lines = [x.strip() for x in lines] 
     subjects = []
     contents = []
     maincats = []
@@ -29,13 +60,10 @@ def get_train_test(train_input, test_input):
             maincats.append(m.group(1))
         else:
             maincats.append('')
-
-    train = pd.DataFrame.from_dict({'subject': subjects, 'content': contents, 'category': maincats})
-
+    train = {'subject': subjects, 'content': contents, 'category': maincats}
     with open(test_input) as f:
         lines = f.readlines()
     lines = [x.strip() for x in lines] 
-
     subjects = []
     contents = []
     for line in lines:
@@ -50,6 +78,5 @@ def get_train_test(train_input, test_input):
         else:
             contents.append('')
         m = re.search(maincat_regex, line)
-
-    test = pd.DataFrame.from_dict({'subject': subjects, 'content': contents})
+    test = {'subject': subjects, 'content': contents}
     return train, test
